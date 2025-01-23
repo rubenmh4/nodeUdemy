d1634df5245ca7402ca64a1ba018b026a2ee9c6d
AYTOS\ruben.moron <Ruben.MORONHINOJOSA@berger-levrault.com>
2025-01-23 23:32:50 +0100

    Avances en proyecto de noc, aviso de log.., services, clean code

--- 05-noc/src/domain/use-cases/checks/check-service.ts
+++ 05-noc/src/domain/use-cases/checks/check-service.ts
@@ -1,10 +1,27 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
 
 interface CheckServiceUseCase {
     execute (url : string) : Promise<boolean>
 }
 
+
+type SuccesCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
+
+
+
 //tenemos un servicio que comprueba la url cada cierto tiempo si no response lanza error
 export class CheckService implements CheckServiceUseCase{
+
+    constructor(
+        private readonly logRepository : LogRepository,
+        private readonly successCallback : SuccesCallback,
+        private readonly errorCallback : ErrorCallback
+    ){
+
+    }
 
     public async execute (url: string ) : Promise<boolean> {
 
@@ -13,12 +30,14 @@ export class CheckService implements CheckServiceUseCase{
             if(!req.ok){
                 throw new Error(`Error on check service ${url}`)
             }
-            console.log(`${url} is ok`)
+            const log = new LogEntity(`Service ${url} working`,LogSeverityLevel.low)
+            this.logRepository.saveLog(log)
+            this.successCallback()
             return true
         } catch (error) {
-            
-            console.log(`${error}`)
-
+            const log = new LogEntity(`Service ${url} not working`,LogSeverityLevel.high)
+            this.logRepository.saveLog(log)
+            this.errorCallback(`${error}`)           
 
             return false
         }
--- 05-noc/src/presentation/server.ts
+++ 05-noc/src/presentation/server.ts
@@ -1,6 +1,15 @@
 
 import { CheckService } from "../domain/use-cases/checks/check-service"
+import { FileSystemDatasouce } from "../infrastructure/datasources/file-system.datasource"
+import { LogRepositoryImpl } from "../infrastructure/repository/log.repository"
 import { CronService } from "./cron/cron-service"
+
+
+//aqui creamos las instancias de las implementaciones
+
+const fileSystemRepository = new LogRepositoryImpl(
+    new FileSystemDatasouce()
+)
 
 
 export class Server {
@@ -10,8 +19,7 @@ export class Server {
 
         CronService.createJob('*/5 * * * * *',()=>{
             
-            new CheckService().execute('https://google.com')
-
+            new CheckService(fileSystemRepository,()=>console.log('success'),(e)=>console.log(e)).execute('https://google.com')
 
         })
     }
